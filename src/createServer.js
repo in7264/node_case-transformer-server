150d209c94a2221640c40170b4037e6802354414
in7264 <in7264@gmail.com>
2026-03-06 23:20:50 +0200

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -36,7 +36,7 @@ function createServer() {
     }
 
     if (errors.length > 0) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.writeHead(400, 'Bad Request', { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({ errors }));
 
       return;
